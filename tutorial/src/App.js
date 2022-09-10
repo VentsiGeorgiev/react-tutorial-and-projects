07e760f7004ea78c79d4fd843f8fd8b584306978
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-10 16:08:35 +0200

    React Tutorial

---
+++ tutorial/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section className='container'>
+      <h1>Tutorial</h1>
+
+      <section className='example'>
+        <h2>Examples</h2>
+      </section>
+    </section>
+  );
+}
+
+export default App;
