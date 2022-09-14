78c0cc2881207859d7272c803ca590f06ff1d951
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-14 08:33:34 +0200

    Mobile menu

--- projects/stripe-clone/src/components/MobileMenu/MobileMenu.js
+++ projects/stripe-clone/src/components/MobileMenu/MobileMenu.js
@@ -1,15 +1,44 @@
 import { useGlobalContext } from '../../context/context';
+import styles from './MobileMenu.module.css';
+import { AiOutlineClose } from 'react-icons/ai';
+import navLinksData from '../../data/data';
 
 function MobileMenu() {
 
     const { isMobileMenuOpen, closeMobileMenu } = useGlobalContext();
 
-    console.log(isMobileMenuOpen);
     return (
-        <div>
-            {isMobileMenuOpen ? <p>True</p> : <p>False</p>}
-            <button onClick={closeMobileMenu}>Close</button>
-        </div>
+        <>
+            {isMobileMenuOpen &&
+                <div className={styles['mob-menu']}>
+                    <ul>
+                        {navLinksData.map((item) => {
+                            const { page, pageUrl, links } = item;
+
+                            return (
+                                <>
+                                    <li className={styles['page-link-li']} >
+                                        <a className={styles['page-link']} href={pageUrl}>{page}</a>
+                                    </li>
+
+                                    <div className={styles['links-wrapper']}>
+                                        {links.map((item) => {
+                                            const { icon, label, url } = item;
+                                            return (<li>
+                                                <a className={styles['page-sub-link']} href={url}>{icon} {label}</a>
+                                            </li>);
+                                        })}
+                                    </div>
+                                </>
+                            );
+
+
+                        })}
+                    </ul>
+                    <button className={`${styles['bnt-close']} btn`} onClick={closeMobileMenu}><AiOutlineClose /></button>
+
+                </div>}
+        </>
     );
 }
 
--- projects/stripe-clone/src/data/data.js
+++ projects/stripe-clone/src/data/data.js
@@ -1,11 +1,21 @@
-import { FaCreditCard, FaBook, FaBriefcase } from 'react-icons/fa';
+import { FaCarrot } from 'react-icons/fa';
+import { MdOutlinePayments, MdPayment, MdPayments, MdDeveloperMode } from 'react-icons/md';
+import { AiOutlineShoppingCart, } from 'react-icons/ai';
+import { BiRefresh } from 'react-icons/bi';
+import { HiTemplate } from 'react-icons/hi';
+import { FaPhoneSquare, FaPlusSquare, FaReceipt, FaGripLines, FaShoppingBasket } from 'react-icons/fa';
+
+
+
 import React from 'react';
 const navLinksData = [
     {
         page: 'Products',
         pageUrl: '/products',
         links: [
-            { label: 'Payments', icon: <FaCreditCard />, url: '/products/payments' },
+            { label: 'Payments', icon: <MdOutlinePayments className='link-icon' />, url: '/products/payments' },
+            { label: 'Checkout', icon: <MdPayments className='link-icon' />, url: '/products/checkout' },
+            { label: 'Elements', icon: <MdPayment className='link-icon' />, url: '/products/elements' },
 
         ],
     },
@@ -13,26 +23,29 @@ const navLinksData = [
         page: 'Solutions',
         pageUrl: '/solutions',
         links: [
-            { label: 'Ecommerce', icon: <FaCreditCard />, url: '/solutions/ecommerce' },
+            { label: 'Ecommerce', icon: <AiOutlineShoppingCart className='link-icon' />, url: '/solutions/ecommerce' },
+            { label: 'SaaS', icon: <BiRefresh className='link-icon' />, url: '/solutions/saas' },
         ],
     },
     {
         page: 'Developers',
         pageUrl: '/developers',
         links: [
-            { label: 'Documentation', icon: <FaBook />, url: '/developers/documentation' },
+            { label: 'Prebuilt checkout', icon: <MdDeveloperMode className='link-icon' />, url: '/developers/prebuilt-checkout' },
+            { label: 'Libraries and SDKs', icon: <HiTemplate className='link-icon' />, url: '/developers/documentation' },
+            { label: 'Plugins', icon: <FaCarrot className='link-icon' />, url: '/developers/documentation' },
         ],
     },
     {
         page: 'Resources',
         pageUrl: '/resources',
         links: [
-            { label: 'Support Center', icon: <FaBriefcase />, url: '/resources/support-center' },
+            { label: 'Support Center', icon: <FaPhoneSquare className='link-icon' />, url: '/resources/support-center' },
+            { label: 'Support Plans', icon: <FaPlusSquare className='link-icon' />, url: '/resources/support-plans' },
+            { label: 'Guides ', icon: <FaReceipt className='link-icon' />, url: '/resources/guides' },
+            { label: 'Customer Stories  ', icon: <FaGripLines className='link-icon' />, url: '/resources/customer-stories ' },
+            { label: 'Contact Sales', icon: <FaShoppingBasket className='link-icon' />, url: '/resources/contact-sales  ' },
         ],
-    },
-    {
-        page: 'Pricing',
-        pageUrl: '/pricing',
     },
 ];
 
