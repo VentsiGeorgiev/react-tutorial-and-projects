c2bba2ca39f26929d3b8c2f401862f0c876fa5fb
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-16 15:58:25 +0200

    Add React Router

--- projects/cocktails/src/App.js
+++ projects/cocktails/src/App.js
@@ -1,6 +1,13 @@
+import { Routes, Route, } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <h1>Cocktails API</h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
   );
 }
 
