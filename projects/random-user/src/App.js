6531145de11036776ac00612c7e064ca27e35eb4
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-17 10:23:37 +0200

    Fetching user

--- projects/random-user/src/App.js
+++ projects/random-user/src/App.js
@@ -1,6 +1,53 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const url = 'https://randomuser.me/api/';
+
 function App() {
+
+    const [user, setUser] = useState({});
+
+    const fetchUser = async () => {
+        try {
+            const response = await fetch(url);
+            const result = await response.json();
+
+            const data = result.results[0];
+            console.log('data');
+            console.log(data);
+
+            const newUser = {
+                firstName: data.name.first,
+                lastName: data.name.last,
+                email: data.email,
+                age: data.dob.age,
+                // address: `${data.location.street.name} ${data.location.street.number} ${data.location.country}`,
+                streetName: data.location.street.name,
+                streetNumber: data.location.street.number,
+                country: data.location.country,
+                phone: data.phone,
+                password: data.login.password,
+                image: data.picture.medium
+            };
+
+            setUser(newUser);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
-        <h1>Random user</h1>
+        <main>
+            <section>
+
+            </section>
+        </main>
     );
 }
 
