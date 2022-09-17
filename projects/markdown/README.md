283a335a4603249d099f51cf9188ecc43ab5fc5f
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-17 08:14:08 +0200

    Markdown app

--- projects/markdown/README.md
+++ projects/markdown/README.md
@@ -1,1 +1,8 @@
 # Markdown
+
+Markdown is a text-to-HTML conversion tool for web writers.
+
+## Live Demo [Markdown](https://app.netlify.com/sites/markdown-f6a1ca/settings/general)
+
+A quick reference to the Markdown syntax.
+https://www.markdownguide.org/cheat-sheet/
--- projects/markdown/src/App.js
+++ projects/markdown/src/App.js
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
 function App() {
+    const [markdown, setMarkdown] = useState('## Markdown Preview');
+
     return (
-        <h1>Markdown</h1>
+        <main>
+            <textarea
+                type='input'
+                value={markdown}
+                onChange={(e) => setMarkdown(e.target.value)}
+            ></textarea>
+
+            <section>
+                <ReactMarkdown>{markdown}</ReactMarkdown>
+            </section>
+        </main>
     );
 }
 
