66794cbbd2537fc269960c36bb7d88d7b393136d
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-19 11:52:28 +0200

    Fetch Quiz Questions

--- projects/quiz/src/context.js
+++ projects/quiz/src/context.js
@@ -1,17 +1,37 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AppContext = createContext();
 
-const AppProvider = ({ children }) => {
+const API_URL = 'https://opentdb.com/api.php?';
 
+
+const AppProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [questions, setQuestions] = useState([]);
     const [formData, setFormData] = useState({
         questionCount: 10,
-        category: 'sport',
+        category: 'sports',
         difficulty: 'easy',
     });
     const { questionCount, category, difficulty } = formData;
 
+    const fetchData = async (quesCount, cat, diff) => {
+        const categoryConfig = {
+            sports: 21,
+            mythology: 20,
+            history: 23,
+        };
+        try {
+            const response = await fetch(`${API_URL}amount=${quesCount}&category=${categoryConfig[cat]}&difficulty=${diff}&type=multiple`);
+            const result = await response.json();
+            // console.log(result.results);
+            setQuestions(result.results);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
 
+    };
 
     const handleChange = (e) => {
         // console.log(e.target.value);
@@ -23,15 +43,18 @@ const AppProvider = ({ children }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('---');
-        console.log(questionCount);
-        console.log(category);
-        console.log(difficulty);
-        console.log('---');
+
+        if (!questionCount || !category || !difficulty) {
+            return;
+        }
+
+        fetchData(questionCount, category, difficulty);
     };
 
 
     return <AppContext.Provider value={{
+        isLoading,
+        questions,
         formData,
         handleChange,
         handleSubmit
