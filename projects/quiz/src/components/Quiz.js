4842101b6f12ecab942abb6a5aa56001bcdd1076
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-19 14:16:02 +0200

    Shuffle Answers

--- projects/quiz/src/components/Quiz.js
+++ projects/quiz/src/components/Quiz.js
@@ -12,6 +12,11 @@ function Quiz() {
         const { question, incorrect_answers, correct_answer } = questions[page];
 
         const allAnswers = [...incorrect_answers, correct_answer];
+        const suffledAnswers = allAnswers
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value);
+
 
         return (
             <div className="section quiz-section">
@@ -20,7 +25,7 @@ function Quiz() {
                         <h2 className='question-title'>{question}</h2>
                         <div className='answers'>
                             <p className='correct-answers'>Correct Answers:  {correct}/{page}</p>
-                            {allAnswers.map((answer, index) => (
+                            {suffledAnswers.map((answer, index) => (
                                 <button onClick={() => checkAnswer(correct_answer === answer)} className='btn-answer' key={index}>{answer}</button>
                             ))}
                         </div>
